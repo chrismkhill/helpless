a90f688f72b4380eee00147784bbc0dee4b88b0f
Dorian Dargan <doriandargan@Dorians-MacBook-Air.local>
2016-03-26 14:52:37 -0400

    Dorian testing out first commit.
    I added a comment to test out the commit.

---
+++ Assets/SCENES/FirstCommit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstCommit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Hi, this is Dorian testing out our Github commits! :)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

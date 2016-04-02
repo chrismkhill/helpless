1dc321d938853c350bea0c9016829899dd7eb1bd
Rehmat Qadir <rehmatqadir@gmail.com>
2016-04-02 16:20:11 -0400

    imported floorplan, networking components, app ids, tested firebase connectivity
    Firebase and Photon plugins added and configured with App ids,
    connectivity tested for Firebase. Basic network test scripts added.
    Floorplan imported as .obj file.
    
    Added folder structure to project for organization. All changes are
    visible in “Sandbox_v02”

---
+++ Assets/Scripts/AppConstants.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class AppConstants : MonoBehaviour
+{
+	public static string kFirebaseAppID = "helpless.firebaseIO.com";
+	public static string kFirebaseSecret = "zApXtAglfl8eMAWM5y4vA5X2WdSDiOD33PW5PGkC";
+	public static string kPhotonVoiceAppID = "ba55f314-02b2-4c9f-9757-67c61b1e509b";
+
+	IFirebase fbRootRef;
+	IFirebase primaryRef;
+
+	void Awake ()
+	{
+		fbRootRef = Firebase.CreateNew ("https://" + kFirebaseAppID);
+		primaryRef = fbRootRef.Child ("initChild");
+	}
+
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void OnEnable ()
+	{
+		primaryRef.ChildAdded += primaryRefChildAdded;
+	}
+
+	void OnDisable ()
+	{
+		primaryRef.ChildAdded -= primaryRefChildAdded;
+	}
+
+	void primaryRefChildAdded (object sender, FirebaseChangedEventArgs e)
+	{
+		Debug.Log ("childAdded: " + e.DataSnapshot.Key);
+	}
+	
+
+}

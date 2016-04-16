97d6d5e84392fda9abd6bf6ac1116c54bc290b24
Chris <chrismkhill@gmail.com>
2016-04-16 16:23:52 -0400

    Added Dual Cam on Player
    On the Player in Scene_02, hit “Activate Dual Cam” to have both the top
    view and player view activated, god view follows the player which I
    added a blue cube

---
+++ Assets/Scripts/DualCamera.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DualCamera : MonoBehaviour 
+{
+	
+
+	public Camera playerCamera;
+
+	public Camera godCamera; 
+
+	public bool activeDualCamera;
+
+
+
+	public void Update()
+	{
+		if (activeDualCamera)
+		{
+			godCamera.transform.position = new Vector3(playerCamera.transform.position.x, godCamera.transform.position.y, playerCamera.transform.position.z);
+		}
+	}
+
+
+	public void Activate()
+	{
+		activeDualCamera = true;
+		playerCamera.rect = new Rect(0f, 0f, .5f, 1f);
+		godCamera.rect = new Rect(.5f, 0f, .5f, 1f);
+
+		godCamera.enabled = true;
+
+	}
+
+
+
+	public void Inactive()
+	{
+		activeDualCamera = false;
+		playerCamera.rect = new Rect(0f, 0f, 1f, 1f);
+		godCamera.rect = new Rect(0f, 0f, 1f, 1f);
+		godCamera.enabled = false;
+	}
+
+
+
+
+}

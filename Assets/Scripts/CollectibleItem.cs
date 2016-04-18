f33a20b46771364f16d4580248ec86f745c090e4
Chris <chrismkhill@gmail.com>
2016-04-18 17:24:43 -0400

    Added collectibles
    Can now collect 5 collectibles with GUI Text and the game restarts

---
+++ Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleItem : MonoBehaviour {
+
+    public GUIManagement guiManagement;
+
+    public void Start()
+    {
+        guiManagement = GameObject.Find("GUIManagement").GetComponent<GUIManagement>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+            guiManagement.collectiblesFound ++;
+        }
+        guiManagement.CheckForWin();
+    }
+}

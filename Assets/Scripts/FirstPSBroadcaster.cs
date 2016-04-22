21fb40fccacfd46209d3fa13817750d2b15525a2
Rehmat Qadir <rehmatqadir@gmail.com>
2016-04-22 11:24:57 -0400

    updates to fb and plugins

---
+++ Assets/Scripts/FirstPSBroadcaster.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FirstPSBroadcaster : MonoBehaviour
+{
+
+	private Transform currentTransform;
+	public HelplessFBController fbController;
+	private IFirebase rootRef;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+		rootRef = fbController.primaryRef;
+//		firebaseRoom = new IFirebase ();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		currentTransform = this.gameObject.transform;
+
+		//rootRef.
+	}
+}

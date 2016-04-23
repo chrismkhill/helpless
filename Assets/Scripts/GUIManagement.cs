46008f7a64e9bbd630615a942d56c6bfd60e0020
Chris <chrismkhill@gmail.com>
2016-04-23 15:14:07 -0400

    Added a timer

--- Assets/Scripts/GUIManagement.cs
+++ Assets/Scripts/GUIManagement.cs
@@ -7,18 +7,26 @@ public class GUIManagement : MonoBehaviour
     public int collectiblesCount = 5;
     public int collectiblesFound = 0;
 
+    public float timerInSeconds = 300f;
+
     public Text scoreText;
+    public Text timerText;
 
 	// Use this for initialization
 	void Start () 
     {
         scoreText.text = collectiblesFound.ToString() + "/" + collectiblesCount.ToString();
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	
+	    timerInSeconds -= Time.deltaTime;
+        int timerMinutes = (int)timerInSeconds / 60;
+        int timerSeconds = (int)timerInSeconds % 60;
+        timerText.text = timerMinutes.ToString() + ":" + timerSeconds.ToString();
+        
 	}
 
     public void CheckForWin()

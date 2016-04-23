33a2281ffb5d14240bdc8105e4ee3445b5d80584
Chris <chrismkhill@gmail.com>
2016-04-23 13:19:05 -0400

    FPSPlayer Tag

--- Assets/Scripts/CollectibleItem.cs
+++ Assets/Scripts/CollectibleItem.cs
@@ -17,7 +17,7 @@ public class CollectibleItem : MonoBehaviour {
 
     void OnTriggerEnter(Collider other) 
     {
-        if (other.tag == "Player")
+        if (other.tag == "FPSPlayer")
         {
             Destroy(gameObject);
             guiManagement.collectiblesFound ++;

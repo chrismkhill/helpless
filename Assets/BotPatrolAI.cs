7ab6a238fabe6487eb4954294034b638dc8178d0
Rehmat Qadir <rehmatqadir@gmail.com>
2016-05-01 16:33:36 -0400

    nav mesh baking and removing unused scenes.

---
+++ Assets/BotPatrolAI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using System;
+using System.Collections;
+
+
+public class BotPatrolAI : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}

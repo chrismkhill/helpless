42924ee1c104a3d8c3caaee78a35ce08c96e01d5
Rehmat Qadir <rehmatqadir@gmail.com>
2016-05-07 00:05:05 -0400

    bot path testing, parented quad (floor) to floorplan for nav mesh
    bot path testing, parented quad (floor) to floorplan for nav mesh,
    radius changes on colliders to prevent bot getting trapped, path script
    (deactivated until paths determined and furniture placed)

--- Assets/Scripts/BotPatrolAI.cs
+++ Assets/Scripts/BotPatrolAI.cs
@@ -3,60 +3,101 @@ using UnityStandardAssets.Characters.ThirdPerson;
 using System;
 using System.Collections;
 
-
-public class BotPatrolAI : MonoBehaviour
+namespace UnityStandardAssets.Chararacters.ThirdPerson
 {
-	public Transform botGuardingTransform;
-	public Transform[] patrolPoints;
-	public float speed = 5.0f;
-	public int curWayPoint = 0;
-	public float pursueSpeed = 9.0f;
-	private Transform startingPoint;
-	private NavMeshAgent navMeshAgent;
-	public ThirdPersonCharacter character;
-	public GuardState currentState;
-
-
-	public enum GuardState
+	public class BotPatrolAI : MonoBehaviour
 	{
-		PATROLLING,
-		PURSUING
-	}
+		public Transform botGuardingTransform;
+		public Transform[] patrolPoints;
+		public GameObject[] originPoints;
+		public GameObject chaseTarget;
+		public float patrolSpeed = 5.0f;
+		public int curWayPoint = 0;
+		public float pursueSpeed = 9.0f;
+		private Transform startingPoint;
+		private NavMeshAgent navMeshAgent;
+		public ThirdPersonCharacter character;
 
-	// Use this for initialization
-	void Start ()
-	{
-		character = this.gameObject.GetComponent<ThirdPersonCharacter> ();
-		startingPoint = this.gameObject.GetComponent<Transform> ();
-		navMeshAgent = this.gameObject.GetComponent<NavMeshAgent> ();
+		public enum State
+		{
+			PATROLLING,
+			PURSUING
+		}
 
-		currentState = BotPatrolAI.GuardState.PATROLLING;
+		public State currentState;
 
-	}
-
-	void Patrol ()
-	{
-
-	}
-
-	void ChasePlayer ()
-	{
+		bool botAlive = true;
 
 
-	}
 
-	void OnTriggerEnter (Collider collider)
-	{
+		// Use this for initialization
+		void Start ()
+		{
+			character = this.gameObject.GetComponent<ThirdPersonCharacter> ();
+			startingPoint = this.gameObject.GetComponent<Transform> ();
+			navMeshAgent = this.gameObject.GetComponent<NavMeshAgent> ();
+			navMeshAgent.updatePosition = true;
+			navMeshAgent.updateRotation = false;
 
-	}
+			currentState = BotPatrolAI.State.PATROLLING;
+
+			StartCoroutine ("StartStateSequence");
+
+		}
 
 
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
+		IEnumerator StartStateSequence ()
+		{
+			while (botAlive) {
+				switch (currentState) {
+				case State.PATROLLING:
+					Patrol ();
+					break;
+				case State.PURSUING:
+					ChasePlayer ();
+					break;
+				}
+				yield return null;
+			}
+		}
 
+
+		void Patrol ()
+		{
+			navMeshAgent.speed = patrolSpeed;
+			if (Vector3.Distance (this.gameObject.transform.position, originPoints [curWayPoint].transform.position) >= 2) {
+				navMeshAgent.SetDestination (originPoints [curWayPoint].transform.position);
+				character.Move (navMeshAgent.desiredVelocity, false, false);
+			} else if (Vector3.Distance (this.gameObject.transform.position, originPoints [curWayPoint].transform.position) <= 2) {
+				curWayPoint += 1;
+
+				if (curWayPoint > originPoints.Length) {
+					curWayPoint = 0;
+				}
+			} else {
+				character.Move (Vector3.zero, false, false);
+			}
+				
+		}
+
+		void ChasePlayer ()
+		{
+			navMeshAgent.speed = pursueSpeed;
+			navMeshAgent.SetDestination (chaseTarget.transform.position);
+			character.Move (navMeshAgent.desiredVelocity, false, false);
+
+		}
+
+		void OnTriggerEnter (Collider collider)
+		{
+			if (collider.tag == "FPSPlayer") {
+				currentState = BotPatrolAI.State.PURSUING;
+				chaseTarget = collider.gameObject;
+			}
+				
+		}
 
 	}
 }
+
+

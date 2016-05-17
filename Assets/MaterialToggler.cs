f48b13bb2dc0f6ba364e9cda256a2a0898703c8e
Rehmat Qadir <rehmatqadir@gmail.com>
2016-05-17 18:48:57 -0400

    beginning scripts to toggle POV between players and switch corresponding scenery materials.

---
+++ Assets/MaterialToggler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialToggler : MonoBehaviour
+{
+
+	public Material materialToSwitchTo;
+	private Material originalMaterial;
+	public InsideCameraSwitch cSwitch;
+	// Use this for initialization
+	void Start ()
+	{
+		originalMaterial = this.gameObject.GetComponent<Renderer> ().materials [0];
+		cSwitch.toggledCameraEvent += switchMaterial;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void switchMaterial ()
+	{
+		if (this.gameObject.GetComponent<Renderer> ().materials [0] != materialToSwitchTo) {
+
+			this.gameObject.GetComponent<Renderer> ().materials [0] = materialToSwitchTo;
+		} else {
+			this.gameObject.GetComponent<Renderer> ().materials [0] = originalMaterial;
+		}
+	}
+}

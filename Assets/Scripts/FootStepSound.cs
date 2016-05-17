ac771e9a1c1256d41d68d983a0e39d80de5d73c4
Rehmat Qadir <rehmatqadir@gmail.com>
2016-05-17 15:39:49 -0400

    adding footstep sound script, leaving disabled for now until appropriate sound assets found.
    also moving various assets from hierarchy top layer to appropriate
    folders.

---
+++ Assets/Scripts/FootStepSound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootStepSound : MonoBehaviour
+{
+
+	public float stepRate = 0.5f;
+	public float stepCoolDown;
+	public AudioClip footStepSound;
+	private AudioSource source;
+
+	void Start ()
+	{
+
+		source = this.gameObject.GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		stepCoolDown -= Time.deltaTime;
+		if ((Input.GetAxis ("Horizontal") != 0f || Input.GetAxis ("Vertical") != 0f) && stepCoolDown < 0f) {
+			source.pitch = 1f + Random.Range (-0.2f, 0.2f);
+			source.PlayOneShot (footStepSound, 0.9f);
+			stepCoolDown = stepRate;
+		}
+	}
+}
